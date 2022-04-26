8b5721f09c3ccf2a257887dbd6919e1766a0ab5d
jherreman <60401056+jherreman@users.noreply.github.com>
2022-04-26 15:56:35 -0600

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.8.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  #tags = {
+  #  Owner = "cbeck"
+  #  Name = "TFCB-Webinar"
+  #}
+}
+
+output "vpcid" {
+  value = aws_vpc.main.id
+}
