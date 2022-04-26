92e1d21ecb7f6246fa628fd97c926b4d18db9c27
jherreman <60401056+jherreman@users.noreply.github.com>
2022-04-26 16:10:28 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -14,10 +14,10 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
-  #tags = {
-  #  Owner = "jherreman"
-  #  Name = "TFCB-Webinar"
-  #}
+  tags = {
+    Owner = "jherreman"
+    Name = "TFCB-Webinar"
+  }
 }
 
 output "vpcid" {
